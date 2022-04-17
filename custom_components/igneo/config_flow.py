22a01271e4be9d38b54df769a9e2fb86459b5b87
Tabisch <tobithieman@gmail.com>
2022-04-17 08:29:34 +0200

    flowprogress

--- custom_components/igneo/config_flow.py
+++ custom_components/igneo/config_flow.py
@@ -1,45 +1,43 @@
-"""Hisense TV config flow."""
-from ast import IsNot
-import json
-from json.decoder import JSONDecodeError
 import logging
+from typing import Any, Dict, Optional
 
 import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.data_entry_flow import FlowResult
+from homeassistant.helpers.entity_registry import (
+    async_entries_for_config_entry,
+    async_get_registry,
+)
 
 from const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
+data_schema = {
+    vol.Required("email"): str,
+    vol.Required("password"): str,
+}
 
 class IgneoFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Igneo config flow."""
 
-    def __init__(self):
-        """Initialize the config flow."""
-        self._mac = None
-        self._name = None
-        self._mqtt_in = None
-        self._mqtt_out = None
-        self._unsubscribe_auth = None
-        self._unsubscribe_sourcelist = None
-
-    async def async_step_user(self, user_input=None):
+    async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None):
         """Handle user step."""
+
+        errors: Dict[str, str] = {}
 
         if user_input is not None:
             #ToDo implement login validation
-            valid = True
+            try:
+                # await validate_auth(user_input[CONF_ACCESS_TOKEN], self.hass)
+                raise ValueError
+            except ValueError:
+                errors["base"] = "auth"
 
-            if valid is True:
-                return self.async_create_entry()
-        data_schema = {
-            vol.Required("email"): str,
-            vol.Required("password"): str,
-        }
+            if not errors:
+                return self.async_create_entry(title="Igneo", data=user_input)
 
         return self.async_show_form(
-            step_id="init", data_schema=vol.Schema(data_schema)
+            step_id="user", data_schema=data_schema, errors=errors
         )
