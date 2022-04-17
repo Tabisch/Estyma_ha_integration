70a5d6831d9d6b2760ec7fee5d41b7a1eb248c99
Tabisch <tobithieman@gmail.com>
2022-04-17 10:14:34 +0200

    pip test

--- custom_components/igneo/__init__.py
+++ custom_components/igneo/__init__.py
@@ -26,4 +26,5 @@ async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.Conf
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
+    
     return True
