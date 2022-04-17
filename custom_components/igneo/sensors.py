6b5c4009cf39ab545071642beb2be2ee3116c1b8
Tabisch <tobithieman@gmail.com>
2022-04-17 11:19:58 +0200

    api test

--- custom_components/igneo/sensors.py
+++ custom_components/igneo/sensors.py
@@ -1,6 +1,9 @@
 import logging
 from datetime import timedelta
+from tokenize import String
 from typing import Any, Callable, Dict, Optional
+
+import EstymaApi
 
 import voluptuous as vol
 
@@ -21,7 +24,8 @@ from homeassistant.helpers.typing import (
 from homeassistant.const import (
     CONF_USERNAME,
     CONF_PASSWORD,
-    CONF_DEVICE_ID
+    CONF_DEVICE_ID,
+    CONF_NAME
 )
 
 from .const import(
@@ -102,9 +106,12 @@ _LOGGER = logging.getLogger(__name__)
 # Time between updating data from GitHub
 SCAN_INTERVAL = timedelta(seconds=30)
 
+CONF_DEVICES = "devices"
+
 IGNEO_DEVICE_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_DEVICE_ID): cv.Number
+        vol.Required(CONF_DEVICE_ID): cv.Number,
+        vol.Optional(CONF_NAME): cv.string
     }
 )
 
@@ -112,27 +119,38 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_USERNAME): cv.string,
         vol.Required(CONF_PASSWORD): cv.string,
-        vol.Required(CONF_DEVICE_ID): vol.All(cv.ensure_list,[IGNEO_DEVICE_SCHEMA])
+        vol.Required(CONF_DEVICES): vol.All(cv.ensure_list,[IGNEO_DEVICE_SCHEMA])
     }
 )
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
     print("")
 
+async def async_setup_platform(hass: HomeAssistantType, config: ConfigType, async_add_entities: Callable, discovery_info: Optional[DiscoveryInfoType] = None,) -> None:
+    """Set up the sensor platform."""
+    Api = EstymaApi(config[CONF_USERNAME],config[CONF_PASSWORD])
+    sensors = [IgneoSensor(Api, repo) for repo in config[CONF_REPOS]]
+    async_add_entities(sensors, update_before_add=True)
+
 
 
 class IgneoSensor(SensorEntity):
 
-    def __init__(self, estymaapi) -> None:
+    def __init__(self, estymaapi, Device_Id, Name) -> None:
         super().__init__()
         self.estymaapi = estymaapi
-        self._name = estymaapi
+        self._name = Name
+        self._Device_Id = Device_Id
         self.attrs: Dict[str, Any] = {}
         self._available = True
 
     @property
     def name(self) -> str:
         return self._name
+
+    @property
+    def device_id(self) -> str:
+        return self._Device_Id
 
     @property
     def device_state_attributes(self) -> Dict[str, Any]:
