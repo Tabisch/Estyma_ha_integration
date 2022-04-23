cacc08214b6708bde6a9a263ac134fcbf01109e8
Tabisch <tobithieman@gmail.com>
2022-04-23 11:32:57 +0200

    update

--- custom_components/igneo/sensor.py
+++ custom_components/igneo/sensor.py
@@ -102,7 +102,6 @@ class IgneoSensor(SensorEntity):
 
         try:
             _LOGGER.info(f"updating {self._name} - {self.attrs[CONF_DEVICE_ID]}")
-            _LOGGER.info(type(await self._estymaapi.getDeviceData(self.attrs[CONF_DEVICE_ID])))
-            self.state = (self._estymaapi.getDeviceData(self.attrs[CONF_DEVICE_ID]))[self._attributename]
+            self._state = (self._estymaapi.getDeviceData(self.attrs[CONF_DEVICE_ID]))[self._attributename]
         except:
             _LOGGER.exception(traceback.print_exc())
