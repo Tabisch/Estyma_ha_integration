fe5bcf3b1de45acdb62bc2adf06cf6ac052ad8e7
Tabisch <tobithieman@gmail.com>
2023-07-26 21:57:29 +0200

    binary sensor fix

--- custom_components/Estyma/__init__.py
+++ custom_components/Estyma/__init__.py
@@ -2,6 +2,8 @@ import logging
 
 from homeassistant import config_entries, core
 
+
+
 from .const import DOMAIN
 
 PLATFORMS = ["sensor","binary_sensor"]
@@ -21,6 +23,14 @@ async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.Conf
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
     # Forward the setup to the sensor platform.
+
+    if "dataUpToDate" in entry.entry_id:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, "binary_sensor")
+        )
+
+        return True
+
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
