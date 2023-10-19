2db6b4ca960db54a5bc236e12269fb4da6854478
Tobias Thiemann <tobithieman@gmail.com>
2023-10-19 09:09:48 +0200

    add dump settingschanges in debug logs

--- custom_components/Estyma/switch.py
+++ custom_components/Estyma/switch.py
@@ -159,6 +159,7 @@ class EstymaBinarySwitch(SwitchEntity):
 
         if await self._estymaapi.isUpdating(self.attrs[CONF_DEVICE_ID], self._attributename):
             _LOGGER.debug(f"updating disabled - entity is updating  {self._name} - {self.attrs[CONF_DEVICE_ID]}")
+            _LOGGER.debug(await self._estymaapi.isUpdating(self.attrs[CONF_DEVICE_ID], self._attributename))
             return
         #else:
         #    _LOGGER.debug(f"updating {self._name} - {self.attrs[CONF_DEVICE_ID]}")
